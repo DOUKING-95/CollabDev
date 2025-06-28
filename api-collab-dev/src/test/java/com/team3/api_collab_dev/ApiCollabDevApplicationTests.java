86d80406a846c9b6c4349171e5f70f7e78e18213
DOUKING-95 <dembeleseydou053@gmail.com>
2025-06-28 21:37:20 +0000

    setup du projet spring-boot

---
+++ api-collab-dev/src/test/java/com/team3/api_collab_dev/ApiCollabDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.api_collab_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCollabDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
