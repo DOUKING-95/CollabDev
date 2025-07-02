a9c253e6b4be3846ea4ef0b195d4fb5846cfea83
HamzaSANMO <hamzasanmo@gmail.com>
2025-07-02 18:48:15 +0000

    local modification to recommande profil

--- api-collab-dev/src/main/java/com/team3/api_collab_dev/ApiCollabDevApplication.java
+++ api-collab-dev/src/main/java/com/team3/api_collab_dev/ApiCollabDevApplication.java
@@ -3,6 +3,7 @@ package com.team3.api_collab_dev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApiCollabDevApplication {
 
