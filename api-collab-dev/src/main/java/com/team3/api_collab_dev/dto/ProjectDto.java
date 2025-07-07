105ef3e388fdf0799e457c94209e169bfefb53fa
DOUKING-95 <dembeleseydou053@gmail.com>
2025-07-07 09:20:03 +0000

    yoo

--- api-collab-dev/src/main/java/com/team3/api_collab_dev/dto/ProjectDto.java
+++ api-collab-dev/src/main/java/com/team3/api_collab_dev/dto/ProjectDto.java
@@ -1,20 +1,23 @@
 package com.team3.api_collab_dev.dto;
 
-import com.team3.api_collab_dev.enumType.Level;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-public class ProjectDTO {
+import com.team3.api_collab_dev.entity.User;
+import com.team3.api_collab_dev.enumType.Domain;
+import jakarta.validation.constraints.NotBlank;
 
-    private Long id;
-    private String title;
-    private Level level;
+public record ProjectDto(
 
-    public ProjectDTO(Long id, String title, Level level) {
-        this.id = id;
-        this.title = title;
-        this.level = level;
-    }
+        @NotBlank
+        String title,
+
+        @NotBlank
+        String description,
+
+        @NotBlank
+        Domain domain,
+        String specification,
+
+        @NotBlank
+        User author) {
+
 }
