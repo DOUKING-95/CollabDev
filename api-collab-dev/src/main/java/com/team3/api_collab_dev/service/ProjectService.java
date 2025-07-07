b9eb954bd5b7f2a50cac0ec989c00862a6f97804
DOUKING-95 <dembeleseydou053@gmail.com>
2025-07-07 09:34:02 +0000

    save after resolve conflict on project dto file

--- api-collab-dev/src/main/java/com/team3/api_collab_dev/service/ProjectService.java
+++ api-collab-dev/src/main/java/com/team3/api_collab_dev/service/ProjectService.java
@@ -1,20 +1,9 @@
 package com.team3.api_collab_dev.service;
 
 
-
-import com.team3.api_collab_dev.dto.ProjectDTO;
+import com.team3.api_collab_dev.dto.ProjectDto;
 import com.team3.api_collab_dev.entity.Project;
 import com.team3.api_collab_dev.enumType.Level;
-import com.team3.api_collab_dev.repository.ProjectRepo;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-
-
-import com.team3.api_collab_dev.entity.Project;
 import com.team3.api_collab_dev.repository.ProjectRepo;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
@@ -32,14 +21,14 @@ public class ProjectService {
     private ProjectRepo projectRepo;
 
 
-    public List<ProjectDTO> filterProjectsByLevel(Level level) {
+  /**  public List<ProjectDto> filterProjectsByLevel(Level level) {
         // Étape 1 : Récupérer tous les projets
         List<Project> projects = (List<Project>) projectRepo.findAll();
 
         // Étape 2 : Filtrer par niveau
-        List<ProjectDTO> filteredProjects = projects.stream()
+        List<ProjectDto> filteredProjects = projects.stream()
                 .filter(project -> project.getLevel() == level)
-                .map(project -> new ProjectDTO(
+                .map(project -> new ProjectDto(
                         project.getId(),
                         project.getTitle(),
                         project.getLevel()
@@ -48,7 +37,7 @@ public class ProjectService {
 
         // Étape 3 : Retourner la liste filtrée
         return filteredProjects;
-    }
+    }*/
 
     public Project saveProject(Project project){
         return  this.projectRepo.save(project);
