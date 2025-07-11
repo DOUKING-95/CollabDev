7bda27a53577808b3d739a2f29ff540258de01e6
Hamza SANMO <hamzasanmo@gmail.com>
2025-07-11 14:24:41 +0000

    after adding task comment by Sekou

---
+++ api-collab-dev/src/main/java/com/team3/api_collab_dev/controller/CommentTaskController.java
@@ -0,0 +1,49 @@
+package com.team3.api_collab_dev.controller;
+
+import com.team3.api_collab_dev.entity.CommentTask;
+import com.team3.api_collab_dev.service.CommentTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("commentTasks")
+public class CommentTaskController {
+
+    @Autowired
+    private CommentTaskService commentTaskService;
+
+    @PostMapping
+    public CommentTask addComment(
+            @PathVariable Long taskId,
+            @RequestBody AddCommenTaskRequest request){
+        return commentTaskService.addComment(taskId, request.getContent(), request.getUserId());
+    }
+
+    public List<CommentTask> getComments(@PathVariable Long taskId){
+        return commentTaskService.getCommentsByTask(taskId);
+    }
+}
+
+//Classe pour gérer la requête POST
+class AddCommenTaskRequest{
+    private String content;
+    private Long userId;
+
+    public String getContent() {
+        return content;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
---
+++ api-collab-dev/src/main/java/com/team3/api_collab_dev/service/CommentTaskService.java
@@ -0,0 +1,60 @@
+package com.team3.api_collab_dev.service;
+
+import com.team3.api_collab_dev.entity.CommentTask;
+import com.team3.api_collab_dev.entity.Project;
+import com.team3.api_collab_dev.entity.Task;
+import com.team3.api_collab_dev.entity.User;
+import com.team3.api_collab_dev.repository.CommentTaskRepo;
+import com.team3.api_collab_dev.repository.ProjectRepo;
+import com.team3.api_collab_dev.repository.TaskRepo;
+import com.team3.api_collab_dev.repository.UserRepo;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentTaskService {
+
+    @Autowired
+    private CommentTaskRepo commentTaskRepo;
+
+    @Autowired
+    private TaskRepo taskRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private ProjectRepo projectRepo;
+
+    public CommentTask addComment(Long taskId, String content, Long userId ){
+        Task task = taskRepo.findById(taskId)
+                .orElseThrow(() -> new EntityNotFoundException("Tâche non trouver avec l'Id : "+taskId));
+        Project project = projectRepo.findById(task.getProject().getId())
+                .orElseThrow(() -> new EntityNotFoundException("Projet non trouvé avec l'Id : "+ task.getProject().getId()));
+        User commenter = userRepo.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("Utilisateur non trouver avec l'Id : "+userId));
+
+        boolean isAuthor = project.getAuthor().getId().equals(userId);
+        boolean isManager = project.getManager() != null && project.getManager().getId().equals(userId);
+        boolean isMember = project.getMembers().stream()
+                .anyMatch(profil -> profil.getUser().getId().equals(userId));
+
+        if (!isAuthor && !isManager && isMember){
+            throw new SecurityException("Seuls l'auteur, le gestionnaire ou les membres peuvent commenter les tâches");
+        }
+
+        if(content == null || content.isBlank()){
+            throw new IllegalArgumentException("Le contenu du commentaire ne peut pas être vide ");
+        }
+
+        CommentTask commentTask = new CommentTask(content, task, commenter);
+        return commentTaskRepo.save(commentTask);
+    }
+
+    public List<CommentTask> getCommentsByTask(Long taskId){
+        return commentTaskRepo.findByTaskId(taskId);
+    }
+}
